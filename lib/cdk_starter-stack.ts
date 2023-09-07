ac274fe4319705cddb4065afa56c14905a3ebe57
Arthur Rabelo <arthurrabelopro@gmail.com>
2023-09-06 22:45:25 -0300

    create 3 basic example S3 buckets by using L1, L2 and L3 constructs

--- lib/cdk_starter-stack.ts
+++ lib/cdk_starter-stack.ts
@@ -1,16 +1,34 @@
 import * as cdk from 'aws-cdk-lib';
+import { Bucket, CfnBucket } from 'aws-cdk-lib/aws-s3';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+
+class L3Bucket extends Construct {
+  constructor(scope: Construct, id: string, expiration: number) {
+    super(scope, id);
+
+    new Bucket(this, 'L3Bucket', {
+      lifecycleRules: [{ expiration: cdk.Duration.days(expiration) }],
+    });
+  }
+}
 
 export class CdkStarterStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    //Create an S3 Bucket - L1
+    new CfnBucket(this, 'MyL1Bucket', {
+      lifecycleConfiguration: {
+        rules: [{ expirationInDays: 1, status: 'Enabled' }],
+      },
+    });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'CdkStarterQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    //Create an S3 Bucket - L2
+    new Bucket(this, 'MyL2Bucket', {
+      lifecycleRules: [{ expiration: cdk.Duration.days(1) }],
+    });
+
+    //Create an S3 Bucket - L3
+    new L3Bucket(this, 'MyL3Bucket', 1);
   }
 }
