33cc44af57be4233b3bc5e783bd751209a9c8f50
Arthur Rabelo <arthurrabelopro@gmail.com>
2023-09-07 13:19:38 -0300

    setup and add a CloudFormation output construct to output the name of our L2 S3Bucket

--- bin/cdk_starter.ts
+++ bin/cdk_starter.ts
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
-import 'source-map-support/register';
-import * as cdk from 'aws-cdk-lib';
-import { CdkStarterStack } from '../lib/cdk_starter-stack';
+import "source-map-support/register";
+import * as cdk from "aws-cdk-lib";
+import { CdkStarterStack } from "../lib/cdk_starter-stack";
 
 const app = new cdk.App();
-new CdkStarterStack(app, 'CdkStarterStack');
+new CdkStarterStack(app, "CdkStarterStack");
--- lib/cdk_starter-stack.ts
+++ lib/cdk_starter-stack.ts
@@ -1,12 +1,12 @@
-import * as cdk from 'aws-cdk-lib';
-import { Bucket, CfnBucket } from 'aws-cdk-lib/aws-s3';
-import { Construct } from 'constructs';
+import * as cdk from "aws-cdk-lib";
+import { Bucket, CfnBucket } from "aws-cdk-lib/aws-s3";
+import { Construct } from "constructs";
 
 class L3Bucket extends Construct {
   constructor(scope: Construct, id: string, expiration: number) {
     super(scope, id);
 
-    new Bucket(this, 'L3Bucket', {
+    new Bucket(this, "L3Bucket", {
       lifecycleRules: [{ expiration: cdk.Duration.days(expiration) }],
     });
   }
@@ -17,18 +17,20 @@ export class CdkStarterStack extends cdk.Stack {
     super(scope, id, props);
 
     //Create an S3 Bucket - L1
-    new CfnBucket(this, 'MyL1Bucket', {
+    new CfnBucket(this, "MyL1Bucket", {
       lifecycleConfiguration: {
-        rules: [{ expirationInDays: 1, status: 'Enabled' }],
+        rules: [{ expirationInDays: 2, status: "Enabled" }],
       },
     });
 
     //Create an S3 Bucket - L2
-    new Bucket(this, 'MyL2Bucket', {
+    const myL2Bucket = new Bucket(this, "MyL2Bucket", {
       lifecycleRules: [{ expiration: cdk.Duration.days(1) }],
     });
 
+    new cdk.CfnOutput(this, "MyL2BucketName", { value: myL2Bucket.bucketName });
+
     //Create an S3 Bucket - L3
-    new L3Bucket(this, 'MyL3Bucket', 1);
+    new L3Bucket(this, "MyL3Bucket", 1);
   }
 }
